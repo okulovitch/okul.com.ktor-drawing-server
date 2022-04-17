c859f9c087f0d3d20eb105d9be84653e800e7d1c
okulovitchVladimir <uladzimir.akulovich@regula.by>
2022-04-17 16:12:35 +0300

    setting up the base structure, add util for drawingServer

---
+++ src/main/kotlin/okul/com/DrawingServer.kt
@@ -0,0 +1,27 @@
+package okul.com
+
+import okul.com.data.Player
+import okul.com.data.Room
+import java.util.concurrent.ConcurrentHashMap
+
+class DrawingServer {
+    val rooms = ConcurrentHashMap<String, Room>()
+    val players = ConcurrentHashMap<String, Player>()
+
+    fun playerJoined(player: Player) {
+        players[player.clientId] = player
+    }
+
+    fun getRoomWithClientId(clientId: String): Room? {
+        val filteredRooms = rooms.filterValues { room ->
+            room.players.find { player ->
+                player.clientId == clientId
+            } != null
+        }
+        return if (filteredRooms.values.isEmpty()){
+            null
+        } else {
+            filteredRooms.values.toList()[0]
+        }
+    }
+}
