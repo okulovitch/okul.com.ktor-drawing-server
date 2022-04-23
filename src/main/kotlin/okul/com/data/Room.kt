b0a1d7892b3f4529b89c615ae379570b1c28d7c2
okulovitchVladimir <uladzimir.akulovich@regula.by>
2022-04-23 18:27:44 +0300

    implement game phases

--- src/main/kotlin/okul/com/data/Room.kt
+++ src/main/kotlin/okul/com/data/Room.kt
@@ -8,6 +8,32 @@ class Room(
     val maxPlayers: Int,
     var players: List<Player> = listOf()
 ) {
+    private var phaseChangedListener: ((Phase) -> Unit)? = null
+    var phase = Phase.WAITING_FOR_PLAYERS
+        set(value) {
+            synchronized(field) {
+                field = value
+                phaseChangedListener?.let { change ->
+                    change(value)
+                }
+            }
+        }
+    private fun setPhaseChangedListener(listener: (Phase) -> Unit) {
+        phaseChangedListener = listener
+    }
+
+    init {
+        setPhaseChangedListener { newPhase ->
+            when (newPhase) {
+                Phase.WAITING_FOR_PLAYERS -> waitingForPlayers()
+                Phase.WAITING_FOR_START -> waitingForStart()
+                Phase.NEW_ROUND -> newRound()
+                Phase.GAME_RUNNING -> gameRunning()
+                Phase.SHOW_WORD -> showWord()
+            }
+        }
+    }
+
     suspend fun broadcast(message: String) {
         players.forEach { player ->
             if (player.socket.isActive) {
@@ -26,5 +52,28 @@ class Room(
 
     fun containsPlayer(userName: String): Boolean {
         return players.find { it.userName == userName } != null
+    }
+
+    private fun waitingForPlayers() {
+
+    }
+    private fun waitingForStart() {
+
+    }
+    private fun newRound() {
+
+    }
+    private fun gameRunning() {
+
+    }
+    private fun showWord() {
+
+    }
+    enum class Phase {
+        WAITING_FOR_PLAYERS,
+        WAITING_FOR_START,
+        NEW_ROUND,
+        GAME_RUNNING,
+        SHOW_WORD
     }
 }
