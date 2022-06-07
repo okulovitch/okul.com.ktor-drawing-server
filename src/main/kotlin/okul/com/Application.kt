9aad1dd8ee30d51f1776dd945a0b17f1f100dcb0
okulovitchVladimir <uladzimir.akulovich@regula.by>
2022-06-07 22:27:31 +0300

    del extra imports

--- src/main/kotlin/okul/com/Application.kt
+++ src/main/kotlin/okul/com/Application.kt
@@ -1,24 +1,16 @@
 package okul.com
 
 import com.google.gson.Gson
-import io.ktor.application.*
-import io.ktor.routing.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
-import io.ktor.sessions.*
-import io.ktor.util.*
-import io.ktor.websocket.*
 import okul.com.plugins.*
-import okul.com.routes.createRoomRoute
-import okul.com.session.DrawingSession
 
 val server = DrawingServer()// used for global access
 val gson = Gson()// used for global access
 fun main() {
     embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
         configureSessions()
-        install(WebSockets)
-        configureRouting()//todo fix
+        configureRouting()
         configureSerialization()
         configureSockets()
         configureMonitoring()
--- src/main/kotlin/okul/com/plugins/Routing.kt
+++ src/main/kotlin/okul/com/plugins/Routing.kt
@@ -3,6 +3,7 @@ package okul.com.plugins
 import io.ktor.routing.*
 import io.ktor.http.*
 import io.ktor.application.*
+import io.ktor.websocket.*
 
 import okul.com.routes.createRoomRoute
 import okul.com.routes.gameWebSocketRoute
@@ -11,10 +12,11 @@ import okul.com.routes.getRoomsRoute
 import okul.com.routes.joinRoomRoute
 
 fun Application.configureRouting() {
+    install(WebSockets)
     install(Routing) {
         createRoomRoute()
         getRoomsRoute()
         joinRoomRoute()
-        gameWebSocketRoute()//todo crash app need fix
+        gameWebSocketRoute()
     }
 }
